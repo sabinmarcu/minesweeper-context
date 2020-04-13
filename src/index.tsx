8a83fdd8a4ebabc5cd83ca5e982ff2d0fd4ebea1
Sabin Marcu <sabinmarcu@gmail.com>
2020-04-13 19:58:45 +0300

    fix: removed zoom prevention

--- src/index.tsx
+++ src/index.tsx
@@ -3,28 +3,28 @@ import { render } from "react-dom";
 
 import App from "./App";
 
-document.addEventListener(
-  "touchmove",
-  function(event) {
-    if (event.scale !== 1) {
-      event.preventDefault();
-    }
-  },
-  false
-);
+// document.addEventListener(
+//   "touchmove",
+//   function(event) {
+//     if (event.scale !== 1) {
+//       event.preventDefault();
+//     }
+//   },
+//   false
+// );
 
-var lastTouchEnd = 0;
-document.addEventListener(
-  "touchend",
-  function(event) {
-    var now = new Date().getTime();
-    if (now - lastTouchEnd <= 300) {
-      event.preventDefault();
-    }
-    lastTouchEnd = now;
-  },
-  false
-);
+// var lastTouchEnd = 0;
+// document.addEventListener(
+//   "touchend",
+//   function(event) {
+//     var now = new Date().getTime();
+//     if (now - lastTouchEnd <= 300) {
+//       event.preventDefault();
+//     }
+//     lastTouchEnd = now;
+//   },
+//   false
+// );
 
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
